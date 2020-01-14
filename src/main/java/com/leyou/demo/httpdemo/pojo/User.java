1c9743d694abd98e4443e2d7a7f66f33bf359a0c
ryz2593 <805445415@qq.com>
2020-01-14 11:09:55 +0800

    http demo

---
+++ src/main/java/com/leyou/demo/httpdemo/pojo/User.java
@@ -0,0 +1,43 @@
+package com.leyou.demo.httpdemo.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author ryz2593
+ * @date 2020/1/13 15:47
+ * <p>
+ * 需要设置表名，以及主键
+ */
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionId = 1L;
+    private Long id;
+
+    private String userName;
+
+    private String password;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer sex;
+
+    private String note;
+
+    private Date birthday;
+
+    private Date creteTime;
+
+    private Date updateTime;
+
+    /**
+     * //如果字段不需要持久化到数据库使用Transient注解修饰
+     * //表示该字段是瞬时的
+     */
+    private String extra;
+}
