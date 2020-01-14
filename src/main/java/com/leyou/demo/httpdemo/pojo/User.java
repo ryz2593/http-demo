ce8e35bdc0040d7bc4319763bddea12f4e4eeed7
ryz2593 <805445415@qq.com>
2020-01-14 16:15:03 +0800

    restTemplate远程调用

--- src/main/java/com/leyou/demo/httpdemo/pojo/User.java
+++ src/main/java/com/leyou/demo/httpdemo/pojo/User.java
@@ -1,7 +1,5 @@
 package com.leyou.demo.httpdemo.pojo;
 
-import lombok.Data;
-
 import java.io.Serializable;
 import java.util.Date;
 
@@ -9,9 +7,8 @@ import java.util.Date;
  * @author ryz2593
  * @date 2020/1/13 15:47
  * <p>
- * 
+ *
  */
-@Data
 public class User implements Serializable {
 
     private static final long serialVersionId = 1L;
@@ -40,4 +37,113 @@ public class User implements Serializable {
      * //表示该字段是瞬时的
      */
     private String extra;
+
+    public static long getSerialVersionId() {
+        return serialVersionId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getCreteTime() {
+        return creteTime;
+    }
+
+    public void setCreteTime(Date creteTime) {
+        this.creteTime = creteTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getExtra() {
+        return extra;
+    }
+
+    public void setExtra(String extra) {
+        this.extra = extra;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", note='" + note + '\'' +
+                ", birthday=" + birthday +
+                ", creteTime=" + creteTime +
+                ", updateTime=" + updateTime +
+                ", extra='" + extra + '\'' +
+                '}';
+    }
 }
--- src/test/java/com/leyou/demo/httpdemo/HttpDemoApplicationTests.java
+++ src/test/java/com/leyou/demo/httpdemo/HttpDemoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.leyou.demo.httpdemo;
 
-import org.junit.jupiter.api.Test;
+import com.leyou.demo.httpdemo.pojo.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class HttpDemoApplicationTests {
+public class HttpDemoApplicationTests {
+    private RestTemplate restTemplate;
+
+    @Before
+    public void init() {
+        restTemplate = new RestTemplate();
+    }
 
     @Test
-    void contextLoads() {
+    public void httpGet() {
+        User user = restTemplate.getForObject("http://localhost:8088/user/2", User.class);
+        System.out.println("user = " + user.toString());
     }
 
 }
